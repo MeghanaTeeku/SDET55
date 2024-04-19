08cd3695986f88843c32efde35a0286736a46689
CSC <CSC@Roopak07>
2024-04-18 16:26:25 -1200

    nnmnnnjdddddddddddddddddddddddddddddd

---
+++ test_cmd/src/test/java/testng_pom/Suites.java
@@ -0,0 +1,16 @@
+package testng_pom;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class Suites {
+	@Test
+	public void one() {
+//		WebDriver x=new FirefoxDriver();
+//		String browser=System.getProperty(null);
+//		String url=System.getProperty("urls");
+		System.out.println("helllooooooooooooooooooo");
+		
+	}
+}
